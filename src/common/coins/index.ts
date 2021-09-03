2a913e642ac92f84a85be7b59cde1cd585210258
⛓️INTERCHAINED ⚡Electronero Network ʕ•̫͡•ʔ-̫͡-ʕ•͓͡•ʔ-̫͡-ʕ•̫͡•ʔ-̫͡-ʔ <evansmark.work@gmail.com>
2021-09-02 20:05:42 -0400

    Update index.ts

--- src/common/coins/index.ts
+++ src/common/coins/index.ts
@@ -1,4 +1,10 @@
 import BTC from './BTC'
+import ETNX from './ETNX'
+import ETNXP from './ETNXP'
+import LTNX from './LTNX'
+import GLDX from './GLDX'
+import CRFI from './CRFI'
+import BABYDOGE from './BABYDOGE'
 import LTC from './LTC'
 import GHOST from './GHOST'
 import NEXT from './NEXT'
@@ -7,7 +13,12 @@ const coins = {
   [BTC.ticker]: BTC,
   [LTC.ticker]: LTC,
   [GHOST.ticker]: GHOST,
-  [NEXT.ticker]: NEXT
+  [NEXT.ticker]: NEXT,
+  [ETNX.ticker]: ETNX,
+  [ETNXP.ticker]: ETNXP,
+  [LTNX.ticker]: LTNX,
+  [GLDX.ticker]: GLDX,
+  [CRFI.ticker]: CRFI
 }
 
 export default coins
